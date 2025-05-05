9709830a027061588e487a262cea500c691a09dd
joshualozano <joshualozano2002@gmail.com>
2025-05-04 21:00:18 -0700

    Code ready to turn in

--- pt-sim-clock.sh
+++ pt-sim-clock.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-echo "Welcome to PT-Sim-Clock, the enhanced page table simulator"
-echo "Using input file $1"
-
-# Run the Python implementation with the clock flag
-./pt_sim.py "$1" --clock
+# Run the Python implementation with clock algorithm
+./pt_sim.py --clock "$1"
 
 # This is where you would run your program
 
